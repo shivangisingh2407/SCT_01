4247af09449a1e753e80aa145a8019007c0c95f6
Shivangi Singh <singhriya8967@gmail.com>
2025-01-16 13:29:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// Select the navigation bar
+const navbar = document.getElementById('navbar');
+
+// Function to change navbar style on scroll
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
+
